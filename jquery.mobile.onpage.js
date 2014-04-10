f149974053023753cd95351c8f76b8adea6350d6
Alexander Schmitz <arschmitz@gmail.com>
2014-04-10 17:20:24 -0400

    Adding offPage to go with onPage

--- jquery.mobile.onpage.js
+++ jquery.mobile.onpage.js
@@ -1,7 +1,13 @@
 (function( $, undefined ) {
 	var globalPagecontainer,
 		events = {},
-		from = [ "pagecontainerbeforechange", "pagecontainerbeforeload", "pagecontainerbeforehide", "pagecontainerhide", "pagecontainerremove" ],
+		from = [
+			"pagecontainerbeforechange",
+			"pagecontainerbeforeload",
+			"pagecontainerbeforehide",
+			"pagecontainerhide",
+			"pagecontainerremove"
+		],
 		deffered = $.Deferred(function( defer ){
 			$.widget( "mobile.pagecontainer", $.mobile.pagecontainer, {
 				_create: function(){
@@ -18,19 +24,18 @@
 			eventNames = eventName.split( " " );
 
 		if( this.length === 0 ) {
-			return;
+			return this;
 		}
 		if ( !delegated ) {
 			callback = element;
 			element = this.selector;
 			pagecontainer = $( this ).closest( ":mobile-pagecontainer" );
 			if( pagecontainer.length === 0 ) {
-				return;
+				return this;
 			}
 		}
 		$.each( eventNames, function( index, event ) {
 			deffered.done( function(){
-				console.log( "process" );
 				if( delegated ) {
 					pagecontainer = globalPagecontainer;
 				}
@@ -42,7 +47,8 @@
 				events[ event ] = [];
 				events[ event ].push({
 					element: element,
-					callback: callback
+					callback: callback,
+					boundTo: this
 				});
 				var options = {};
 				options[ event ] = handleEvent;
@@ -50,18 +56,50 @@
 			} else {
 				events[ event ].push({
 					element: element,
-					callback: callback
+					callback: callback,
+					boundTo: this
 				});
 			}
 		}
 		function handleEvent( event, ui ){
 			var args = arguments;
-			$.each( events[ event.type.replace( /pagecontainer/, "" ) ], function( index, callback ) {
-				if ( from.indexOf( event.type ) !== -1 && ui.prevPage.is( callback.element ) && ui.prevPage !== undefined ) {
+			$.each( events[ event.type.replace( /pagecontainer/, "" ) ],
+			function( index, callback ) {
+				if ( from.indexOf( event.type ) !== -1 && ui.prevPage.is( callback.element ) &&
+				ui.prevPage !== undefined ) {
 					callback.callback.apply( ui.prevPage, args );
 				} else if ( ui.toPage.is( callback.element ) ){
 					callback.callback.apply( ui.toPage, args );
 				}
+			});
+		}
+		return this;
+	};
+	$.fn.offPage = function( removeEvents, selector, handler ) {
+		if( selector === undefined ) {
+			selector = this;
+		}
+		if( removeEvents === undefined ) {
+			checkCallbacks();
+			return this;
+		}
+
+		var eventArray = removeEvents.split( " " );
+		$.each( eventArray, function( i, remove ) {
+			checkCallbacks( remove );
+		});
+		return this;
+
+		function checkCallbacks( remove ) {
+			$.each( events, function( event, callbackArray ) {
+				$.each( callbackArray, function( index, callback ){
+					if( $( selector ).is( callback.element ) &&
+					( remove? ( event === remove ) : true ) &&
+					( handler? ( handler === callback.callback ): true ) ) {
+						var removeIndex = callbackArray.indexOf( callback );
+						array.splice( removeIndex, 1 );
+					}
+				});
 			});
 		}
 	};
